118b9d807724fc61599fb85370fdbc4df81cb842
nik1168 <ngeisser32@gmail.com>
2022-03-24 22:24:55 -0400

    feat: adding factory design pattern

---
+++ src/creational/factory/Rectangle.java
@@ -0,0 +1,9 @@
+package creational.factory;
+import java.io.*;
+public class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
---
+++ src/creational/factory/ShapeFactory.java
@@ -0,0 +1,23 @@
+package creational.factory;
+
+public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+
+        } else if (shapeType.equalsIgnoreCase("TRIANGLE")) {
+            return new Triangle();
+        }
+
+        return null;
+
+    }
+}
